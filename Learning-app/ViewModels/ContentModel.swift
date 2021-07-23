754c6ffff085757405c8b5704c0b329712529408
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2021-07-22 22:50:29 -0500

    - Add assets - Add data files - Add models - parse json in content model - parse style data

--- Learning-app/ViewModels/ContentModel.swift
+++ Learning-app/ViewModels/ContentModel.swift
@@ -8,5 +8,35 @@
 import Foundation
 
 class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init(){
+        getLocalData()
+    }
+    
+    func getLocalData(){
+        //Get a url to the json file
+        guard let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json") else { return }
+        
+        do{
+            //Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonURL)
+            let jsonDecoder = JSONDecoder()
+            //Try to decode the json
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            self.modules = modules
+        }catch{
+            print(error)
+        }
+        
+        //Parse the style data
+        guard let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html") else { return }
+        do{
+            let styleData = try Data(contentsOf: styleUrl)
+            self.styleData = styleData
+        }catch{
+            print(error)
+        }
+    }
 }
--- Learning-app/Views/HomeView.swift
+++ Learning-app/Views/HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
